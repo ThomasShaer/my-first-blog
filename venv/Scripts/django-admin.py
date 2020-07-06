cdc0e0cd9d152c63817340dbdfa02f98c47daa66
ThomasShaer <txs941@student.bham.ac.uk>
2020-07-06 12:48:46 +0100

    removed pycharm files

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Thomas Shaer\Desktop\Uni Work\Year 1\Bridging coursework\Workspace\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
