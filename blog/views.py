f38e0c35e2b18ab8890faea7ccfe25f22a10a4bb
ThomasShaer <txs941@student.bham.ac.uk>
2020-07-15 15:18:23 +0100

    Added blogs

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            print("redirect me pls")
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -42,3 +41,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+	
+	
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+  
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
